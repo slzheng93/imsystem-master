472e7344f4d02dbe6e021504b5f358fdb0d53d1e
Shao <zhengshaoliang1112@gmail.com>
2016-09-18 13:34:38 -0400

    testing 9/18/2016

--- imsystem-master/app/src/main/java/com/example/imsystem/ToDoActivity.java
+++ imsystem-master/app/src/main/java/com/example/imsystem/ToDoActivity.java
@@ -44,7 +44,7 @@ import com.squareup.okhttp.OkHttpClient;
 import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.*;
 
 public class ToDoActivity extends Activity {
-    /** I am here shao */
+    //I am here: shao
     /**
      * Mobile Service Client reference
      */
